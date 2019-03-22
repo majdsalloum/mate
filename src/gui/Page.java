9bad42ca97ed14d1bfbe1eca8af961a6a106a685
Majd Salloum <majdsalloum@outlook.com>
2019-03-22 18:04:49 +0200

    Update Page.java

--- src/gui/Page.java
+++ src/gui/Page.java
@@ -1,19 +1,24 @@
 package gui;
 
 import javafx.scene.Node;
+import javafx.scene.layout.FlowPane;
 
 public class Page {
     protected String path;
     protected Window window;
-
+    protected FlowPane flowPane = new FlowPane();
 
     public void setWindow(Window window) {
         this.window = window;
     }
 
+    public FlowPane getFlowPane() {
+        return flowPane;
+    }
+
     public Node getContent()
     {
-       return null;
+       return flowPane;
     }
 
     public void setPath(String URL) {
