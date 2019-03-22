7a640b6e38eb84baba8c45b80f7c61f8a01c7fef
Majd Salloum <majdsalloum@outlook.com>
2019-03-22 17:47:08 +0200

    Update InternetConnection.java

--- src/network/InternetConnection.java
+++ src/network/InternetConnection.java
@@ -1,24 +1,42 @@
 package network;
 
+import gui.Window;
+import javafx.scene.control.Label;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
 
 public class InternetConnection {
 
-    public InputStream getPage(String urlPath) throws IOException {
+    private Window window;
+
+    InternetConnection(Window window) {
+        this.window = window;
+    }
+
+    public void getPage(String urlPath) throws IOException {
         URL url;
-        try{
-         url= new URL(urlPath);}
-        catch (Exception e)
-         {
+        try {
+            url = new URL(urlPath);
+        } catch (Exception e) {
             String google = "https://www.google.com/search/web?v=1.0&q=";
             String search = urlPath;
             String charset = "UTF-8";
-             url = new URL(google + URLEncoder.encode(search, charset));
+            url = new URL(google + URLEncoder.encode(search, charset));
         }
-        return url.openStream();
+        InputStream inputStream = url.openStream();
 
+        String data = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String line = br.readLine();
+        while (line != null) {
+            data = data + "\n" + line;
+            line = br.readLine();
+        }
+        window.onLoad(data);
     }
 }
