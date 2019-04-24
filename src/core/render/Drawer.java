c97492dcf91f92a459f26d216cef6bb3a71f3d95
shadi-danhash <48592153+shadi-danhash@users.noreply.github.com>
2019-04-24 18:52:33 +0300

    add attributes to drawer

--- src/core/render/Drawer.java
+++ src/core/render/Drawer.java
@@ -4,7 +4,16 @@ package core.render;
 import javafx.scene.image.Image;
 
 public interface Drawer {
+    enum ATTRIBUTES{
+        FONT_BOLD,
+        FONT_ITALIC,
+        FONT_UNDERLINE,
+    }
     public void drawText(String text);
     public void setTitle(String text);
     public void drawImage(Image image);
+    public void useAttribute(ATTRIBUTES attributes);
+    public void unUseAttribute(ATTRIBUTES attributes);
+    public Boolean hasAttribute(ATTRIBUTES attributes);
+    public void drawTable();
 }
--- src/core/render/FXDrawer.java
+++ src/core/render/FXDrawer.java
@@ -11,6 +11,9 @@ import javafx.scene.image.ImageView;
 public class FXDrawer implements Drawer {
     Tab tab;
     Page page;
+    Integer font_bold=0;
+    Integer font_italic=0;
+    Integer font_underline=0;
 
     public FXDrawer(Tab tab, Page page) {
         this.tab = tab;
@@ -36,5 +39,55 @@ public class FXDrawer implements Drawer {
         ImageView imageView=new ImageView(image);
         label.setGraphic(imageView);
         page.getFlowPane().getChildren().add(label);
+    }
+    @Override
+    public Boolean hasAttribute(ATTRIBUTES attributes){
+        switch (attributes)
+        {
+            case FONT_BOLD:
+                return font_bold>0;
+            case FONT_ITALIC:
+                return font_italic>0;
+            case FONT_UNDERLINE:
+                return font_underline>0;
+                default:return false;
+        }
+    }
+    @Override
+    public void useAttribute(ATTRIBUTES attributes)
+    {
+        switch (attributes)
+        {
+            case FONT_BOLD:
+                font_bold++;
+                break;
+            case FONT_ITALIC:
+                font_italic++;
+                break;
+            case FONT_UNDERLINE:
+                font_underline++;
+                break;
+        }
+    }
+    @Override
+    public void unUseAttribute(ATTRIBUTES attributes)
+    {
+        switch (attributes)
+        {
+            case FONT_BOLD:
+                font_bold--;
+                break;
+            case FONT_ITALIC:
+                font_italic--;
+                break;
+            case FONT_UNDERLINE:
+                font_underline--;
+                break;
+        }
+    }
+    @Override
+    public void drawTable()
+    {
+
     }
 }
