5cf328d6dcc5b9e78e8137d2dbf9a6b9fae70a10
shadi-danhash <48592153+shadi-danhash@users.noreply.github.com>
2019-06-10 01:42:21 +0300

    game

--- src/gui/GamePage.java
+++ src/gui/GamePage.java
@@ -1,57 +1,114 @@
 package gui;
 
 import core.exceptions.UnSupportedSaveType;
-import javafx.animation.Animation;
 import javafx.animation.TranslateTransition;
 import javafx.event.EventHandler;
-import javafx.scene.Cursor;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.util.Builder;
 import javafx.util.Duration;
 
+import java.util.Random;
 
 
 public class GamePage extends Page {
-    int score;
-    int lives;
-    int speed;
+
     Node gameNode;
     public GamePage(Window window, String path, String data) {
         super(window, path, data);
         initializeGame();
+        refreshGame();
     }
+    Player player;
+    Label holder;
+    Label drop;
+    double dropX =0, dropY =0, holderX =0, holderY=500;
+    double animationTime=2000;
+    Label scoreLabel;
+    Label livesLabel;
+    ToolBar toolBar;
+    FlowPane gameGrid;
     private void initializeGame() {
-        score = 0;
-        lives = 5;
-        speed = 1;
-        showGame();
-    }
-    private void showGame() {
-        FlowPane flowPane = new FlowPane();
-        Label label  = new Label();
-        label.setGraphic(new ImageView(Images.logo));
-        Button button = new Button("start");
-        button.setOnAction(e->{
-            TranslateTransition translateTransition = new TranslateTransition();
-            translateTransition.setNode(label);
-            translateTransition.setToY(500);
-            translateTransition.setDuration(new Duration(1000));
-            translateTransition.play();
-        });
-        Circle holder = new Circle(5);
+        player = new Player();
+        scoreLabel = new Label("score 0");
+        livesLabel = new Label("lives 0");
+        toolBar = new ToolBar(scoreLabel , livesLabel);
+        gameGrid = new FlowPane();
+        drop = new Label();
+        holder = new Label();
+        drop.setGraphic(new ImageView(Images.getImage("..\\img\\herb.png")));
+        holder.setGraphic(new ImageView(Images.getImage("..\\img\\holder.png")));
         holder.setOnMouseDragged(circleOnMouseDraggedEventHandler);
         holder.setOnMousePressed(circleOnMousePressedEventHandler);
-//        holder.setOnDragDetected((e)->{
-//            func(holder);
-//        });
-        flowPane.getChildren().addAll(label,holder , button);
-        gameNode = flowPane;
+        drop.setTranslateX(dropX);
+        drop.setTranslateY(dropY);
+        holder.setTranslateX(holderX);
+        holder.setTranslateY(holderY);
+        ((FlowPane)gameGrid).getChildren().addAll(drop , holder);
+        gameNode = new VBox(toolBar , gameGrid);
+
     }
+    private void refreshGame()
+    {
+        scoreLabel.setText("score "+player.getScore());
+        livesLabel.setText("lives "+player.getLives());
+        TranslateTransition translateTransition = new TranslateTransition();
+        translateTransition.setNode(drop);
+        translateTransition.setToY(500);
+        translateTransition.setDuration(new Duration(animationTime/player.getSpeedFactor()));
+        translateTransition.play();
+        translateTransition.setOnFinished((t)->{
+            double holderMinX = holder.getTranslateX() + holder.getLayoutX();
+            double holderMaxX = holderMinX + holder.getWidth();
+            double dropMinX = drop.getTranslateX() + drop.getLayoutX();
+            double dropMaxX = dropMinX + drop.getWidth();
+            //System.out.println(holderX + " " + minX + " " + maxX);
+            if ((dropMinX>=holderMinX && dropMinX<=holderMaxX)||
+                    (dropMaxX>=holderMinX && dropMaxX<=holderMaxX))
+            {
+                player.setScore(player.getScore()+1);
+                System.out.println("Ok");
+            }
+            else {player.setLives(player.getLives()-1);
+                System.out.println("No");}
+            if (player.getLives()>0)
+            reLocate();
+            else showLose();
+        });
+    }
+
+    private void showLose(){
+        scoreLabel.setText("score "+player.getScore());
+        livesLabel.setText("lives "+player.getLives());
+        Button button = new Button("Play Again");
+        Label label = new Label("you lose , your score is " + player.getScore());
+        button.setFont(new Font(50));
+        label.setFont(new Font(50));
+        button.setOnAction((e)->{
+            window.setPageAndUpdate(new GamePage(window , "game" ,null));
+        });
+        gameGrid.setAlignment(Pos.CENTER);
+        gameGrid.getChildren().setAll(label , button);
+        gameGrid.setOrientation(Orientation.VERTICAL);
+        button.setAlignment(Pos.CENTER);
+    }
+
+    private void reLocate(){
+      drop.setTranslateY(dropY);
+      Double rand= (new Random().nextDouble()*2000)%1300 ;
+      drop.setTranslateX(rand);
+      refreshGame();
+    }
+
     @Override
     public String toBeSaved() throws UnSupportedSaveType {
         throw new UnSupportedSaveType("game page");
@@ -75,8 +132,8 @@ public class GamePage extends Page {
                 public void handle(MouseEvent t) {
                     orgSceneX = t.getSceneX();
                     orgSceneY = t.getSceneY();
-                    orgTranslateX = ((Circle)(t.getSource())).getTranslateX();
-                    orgTranslateY = ((Circle)(t.getSource())).getTranslateY();
+                    orgTranslateX = ((Node)(t.getSource())).getTranslateX();
+                    orgTranslateY = ((Node)(t.getSource())).getTranslateY();
                 }
             };
     EventHandler<MouseEvent> circleOnMouseDraggedEventHandler =
@@ -85,12 +142,48 @@ public class GamePage extends Page {
                 @Override
                 public void handle(MouseEvent t) {
                     double offsetX = t.getSceneX() - orgSceneX;
-                    double offsetY = t.getSceneY() - orgSceneY;
+                    //double offsetY = t.getSceneY() - orgSceneY;
                     double newTranslateX = orgTranslateX + offsetX;
-                    double newTranslateY = orgTranslateY + offsetY;
-
-                    ((Circle)(t.getSource())).setTranslateX(newTranslateX);
-                    ((Circle)(t.getSource())).setTranslateY(newTranslateY);
+                    //double newTranslateY = orgTranslateY + offsetY;
+                    ((Node)(t.getSource())).setTranslateX(newTranslateX);
+                    //((Circle)(t.getSource())).setTranslateY(newTranslateY);
                 }
             };
+
+
+
+
+
+    class Player{
+        int lives;
+        int score;
+        double speedFactor;
+         Player()
+        {
+            lives = 5;
+            score = 0;
+            speedFactor = 1;
+        }
+         double getSpeedFactor() {
+            int level = score /5;
+            return speedFactor + (level*0.1);
+        }
+
+         int getLives() {
+            return lives;
+        }
+
+         void setLives(int lives) {
+            this.lives = lives;
+        }
+
+         int getScore() {
+            return score;
+        }
+
+         void setScore(int score) {
+            this.score = score;
+        }
+    }
+
 }
