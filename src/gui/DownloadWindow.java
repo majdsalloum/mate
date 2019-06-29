6ab9f4b27468e8c1202dcea393e6da00f9d30c98
ReemRabah <48592236+ReemRabah@users.noreply.github.com>
2019-06-29 08:52:27 +0300

    Update DownloadWindow.java
    DownloadWindows

--- src/gui/DownloadWindow.java
+++ src/gui/DownloadWindow.java
@@ -7,6 +7,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import network.DownloadManger;
 import java.io.File;
@@ -22,14 +23,23 @@ public class DownloadWindow {
 
     public void showDownloadWindow() {
         Stage stage = new Stage();
+        TextField textField=new TextField();
+
+        textField.setFont(Font.font("Segoe Print",15));
         Label label = new Label("drop the link here");
-        label.setTextFill(Color.DARKGREEN);
+        label.setTextFill(Color.GREEN);
+        label.setFont(Font.font("Segoe Print",40));
         Button button = new Button("ok");
-        button.setBackground(new Background(new BackgroundFill(Color.DARKGREEN,null,null)));
+        button.setFont(Font.font("Segoe Print",30));
+        button.setTextFill(Color.WHITE);
+        button.setBackground(new Background(new BackgroundFill(Color.LIMEGREEN,null,null)));
         VBox vBox = new VBox(label,textField,button);
+        vBox.setSpacing(30);
         vBox.setAlignment(Pos.CENTER);
         stage.getIcons().add(Images.getImage("..\\img\\mate.png"));
         stage.setTitle("Download Manger");
+        stage.setWidth(400);
+        stage.setHeight(400);
         Scene scene = new Scene(vBox);
         stage.setScene(scene);
         stage.show();
@@ -60,9 +70,18 @@ public class DownloadWindow {
         stage.getIcons().add(Images.getImage("..\\img\\mate.png"));
         progressBar = new ProgressBar(0);
         Label label1 = new Label("file name : " + downloadManger.getFile().toString());
+        label1.setFont(Font.font("Segoe Print",30));
+        label1.setTextFill(Color.GREEN );
         Label label2 = new Label("downloaded "+ 0 + "KB from " +size/1024 + "KB");
+        label2.setFont(Font.font("Segoe Print",20));
+        label2.setTextFill(Color.GREEN );
         Button cancel = new Button("cancel");
+        cancel.setTextFill(Color.WHITE );
+        cancel.setBackground(new Background(new BackgroundFill(Color.LIMEGREEN,null,null)));
+
         VBox vBox= new VBox(label1,label2,progressBar,cancel);
+        vBox.setSpacing(30);
+        vBox.setPrefSize(450,400);
         vBox.setMinSize(200,100);
         Scene scene = new Scene(vBox);
         vBox.setAlignment(Pos.CENTER);
